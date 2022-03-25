e488c13791a96af692033deeb3584c35fd18cbc7
Hardik Parmar <hardik.parmar5@gmail.com>
2022-03-25 18:38:47 +0530

    create usser push and routing in app

--- app3.js
+++ app3.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const members = require("./Members");
+// const members = require("./Members");
+const bodyparser = require("body-parser");
 const app = express();
 
-//get all members
-app.get("/api/members", (req, res) => res.json(members));
-//get single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `no member with the id of ${req.params.id}` });
-  }
-});
+//bodyparser middleware
+app.use(express.json());
+//false means object in the form of string or arraay //otherwise true if it is nested object
+app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 8000;
+// //get all members
+// app.get("/api/members", (req, res) => res.json(members));
+// //get single member
+// app.get("/api/members/:id", (req, res) => {
+//   const found = members.some((member) => member.id === parseInt(req.params.id));
+//   if (found) {
+//     res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+//   } else {
+//     res.status(400).json({ msg: `no member with the id of ${req.params.id}` });
+//   }
+// });
+app.use("/api/members", require("./routes/api/members"));
+
+const PORT = 8000;
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
