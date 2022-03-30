2900ad353f9bed93545e642a7047cefffd732235
Hardik Parmar <hardik.parmar5@gmail.com>
2022-03-30 14:16:21 +0530

    practising json() types

--- req_body.js
+++ req_body.js
@@ -3,12 +3,23 @@ const app = express();
 
 //for parsing json
 app.use(express.json()); //diffrent options
-//for parsing app
-app.use(express.urlencoded({ extended: true })); //bodyparser for html post 
+// app.use(express.json({limit : 1})); 
 
+//another way of using parameters
+app.use(express.json({
+    extended: true,             //qs(true) or querystring(false)
+    inflate: true,              //controls compressed/deflected bodies if it is false it will reject 
+    limit: '100kb',             //maximum body size 
+    parameterLimit: 1000,       //controls maximum numbers of fields that we'll attempt to parse from the url
+    type: 'application/x-www-form-urlencoded',  //control content-type. value can be string array functions 
+    verify: undefined           //it is an optional fn which can be used to abort parsing by throwing an error
+  }))
+
+//for parsing app
+app.use(express.urlencoded({ extended: true })); //bodyparser for html post
 
 app.post("/profile", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
   res.send();
 });
 
